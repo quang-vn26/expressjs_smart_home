007a18f4c6cced12bc6ffe767a1a2aedc5adb9de
quangg <nnquang.code@gmail.com>
2020-08-07 22:18:11 +0700

    complete bai 2

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express = require('express')
 var app = express()
 var port = 3000;
+app.set('view engine','pug')
+app.set('views','./views')
+
+ // app.get('/',function(req,res){
+ //  res.send('Hello coderx')
+ // })
  app.get('/',function(req,res){
-  res.send('Hello coderx')
+  res.render('index',{
+    name: 'QQQ'
+  })
  })
 
+// Iteration
+
+ app.get('/users',function(req,res){
+  res.render('users/index',{
+    users:[
+    {id:1,name:'Quang'},
+    {id:2,name:'Quang2'},
+    {id:3,name:'Quang3'}
+    ]
+  })
+ })
  app.listen(port,function(){
   console.log('open in port: '+port)
  })
