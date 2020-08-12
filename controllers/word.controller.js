15e853a3ca9160f5762889babc9c043518a5058a
quangg <nnquang.code@gmail.com>
2020-08-12 20:42:17 +0700

    completed word controller

---
+++ controllers/word.controller.js
@@ -0,0 +1,50 @@
+var shortid = require('shortid');
+var db = require('../db')
+
+module.exports.index = function(req,res){
+  res.render('words/index',{
+    words:db.get('words').value()
+  })
+ }
+
+module.exports.search =  (req,res) => {
+  console.log(req.query);
+  var name_search = req.query.q 
+  var words = db.get('words').value()
+  var result = words.filter( (word) => {
+    return word.name.toLowerCase().indexOf(name_search.toLowerCase()) !== -1
+  })
+
+  res.render('words/index', {
+    words: result // render lại trang words/index với biến words bây giờ chỉ bao gồm các kết quả phù hợp
+  });
+}
+
+
+module.exports.create = function(req,res){
+  res.render('words/create')
+}
+
+module.exports.view_word = function(req,res){
+  var id = req.params.id
+  var word = db.get('words').find({id:id}).value()
+  res.render('words/view',{
+    word:word
+  })
+}
+
+module.exports.delete = function(req,res){
+    // Route parameters: thong so duong truyen
+  var id = req.params.id
+  var word = db.get('words').find({id:id}).value()
+  var word = db.get('words').remove({id:id}).write()
+  res.redirect('/words')
+}
+
+module.exports.postCreate = function(req,res){
+    req.body.id = shortid.generate();
+    req.body.date = new Date().toDateString()
+    //req.body.id = shortid.generate();
+    db.get('words').push(req.body).write()
+    res.redirect('/words');
+}
--- routes/words.route.js
+++ routes/words.route.js
@@ -1,55 +1,12 @@
 var express = require('express')
-var shortid = require('shortid');
-var db = require('../db')
-
 var router = express.Router()
+var word_controller = require('../controllers/word.controller.js')
 
-router.get('/',function(req,res){
-  res.render('words/index',{
-    words:db.get('words').value()
-  })
- })
+router.get('/', word_controller.index)
+router.get('/search', word_controller.search)
+router.get('/create', word_controller.create)
+router.get('/:id', word_controller.view_word)
+router.get('/delete/:id', word_controller.delete)
 
- router.get('/search', (req,res) => {
-  console.log(req.query);
-  var name_search = req.query.q 
-  var words = db.get('words').value()
-  var result = words.filter( (word) => {
-    return word.name.toLowerCase().indexOf(name_search.toLowerCase()) !== -1
-  })
-
-  res.render('words/index', {
-    words: result // render lại trang words/index với biến words bây giờ chỉ bao gồm các kết quả phù hợp
-  });
-})
-
-
- router.get('/create',function(req,res){
-  res.render('words/create')
-})
-
-  router.get('/:id',function(req,res){
-  var id = req.params.id
-  var word = db.get('words').find({id:id}).value()
-  res.render('words/view',{
-    word:word
-  })
-})
-
-//luu y duong dan va view
-  router.get('/delete/:id',function(req,res){
-    // Route parameters: thong so duong truyen
-  var id = req.params.id
-  var word = db.get('words').find({id:id}).value()
-  var word = db.get('words').remove({id:id}).write()
-  res.redirect('/words')
-})
-
-
-  router.post('/create',function(req,res){
-    req.body.id = shortid.generate();
-    req.body.date = new Date().toDateString()
-    db.get('words').push(req.body).write()
-    res.redirect('/words');
-})
+router.post('/create', word_controller.postCreate)
 module.exports = router
