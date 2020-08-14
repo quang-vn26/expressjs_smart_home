4346f4b2f804526288a713cf5033fe5d851c297a
quangg <nnquang.code@gmail.com>
2020-08-14 11:40:55 +0700

    products model using mongodb

---
+++ notes/connectmongo.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+
+// const url = "mongodb+srv://<username>:<password>@clustername.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+const url = "mongodb+srv://quanggdb:EGQdGnZmsy5HUFJ@clusterq.av0iy.gcp.mongodb.net/db?retryWrites=true&w=majority"
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
